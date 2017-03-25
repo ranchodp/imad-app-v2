df4c6646546e09380af469eee01f7d744d8f930b
ranchodp <ranchodp0497@gmail.com>
2017-03-25 09:02:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/', function (req, res) {
 app.get('article-one',function(req,res){
     res.send('article one is requested');
 });
-app.get('article-one',function(req,res){
+app.get('article-two',function(req,res){
     res.send('article one is requested');
 });
-app.get('article-one',function(req,res){
+app.get('article-three',function(req,res){
     res.send('article one is requested');
 });
 app.get('/ui/style.css', function (req, res) {
