c65e6779e1dbfa77c84b44d2f525982f2d7735d5
ranchodp <ranchodp0497@gmail.com>
2017-03-25 09:02:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('article one is requested');
 });
-app.get('article-two',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('article one is requested');
 });
-app.get('article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('article one is requested');
 });
 app.get('/ui/style.css', function (req, res) {
